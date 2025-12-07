02073e355f0717974e5abe2dd090499cc0e6eac5
Abdulla Alshamsi <abdualsh@lab1r1s6.42abudhabi.ae>
2025-12-07 15:36:49 +0400

    create process file to fixed any errer and write on ft_printf.c

--- main.c
+++ main.c
@@ -6,18 +6,29 @@
 /*   By: abdualsh <abdualsh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/02 00:33:19 by abdualsh          #+#    #+#             */
-/*   Updated: 2025/11/15 17:29:05 by abdualsh         ###   ########.fr       */
+/*   Updated: 2025/12/07 13:01:53 by abdualsh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include/ft_printf.h"
 #include <stdio.h>
 
-
-
-int main(void)
+void print_numbers(int count, ...)
 {
-    printf("hello");
-    printf("%s", NULL);
-    
+    va_list args;              // create list
+    va_start(args, count);     // start reading after "count"
+
+    for (int i = 0; i < count; i++)
+    {
+        int x = va_arg(args, int);   // get next number
+        printf("%d ", x);
+    }
+
+    va_end(args);              // finish
+}
+
+int main()
+{
+    print_numbers(3, 10, 20, 30);
+    return 0;
 }
--- src/core/ft_printf.c
+++ src/core/ft_printf.c
@@ -6,26 +6,55 @@
 /*   By: abdualsh <abdualsh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/01 17:30:06 by abdualsh          #+#    #+#             */
-/*   Updated: 2025/11/15 17:20:52 by abdualsh         ###   ########.fr       */
+/*   Updated: 2025/12/07 15:20:06 by abdualsh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "ft_printf.h"
 
-static char *make_malloc
+static int data_processing(va_list args,int total,char copy)
+{
+	int test;
+
+	test = 0;
+	if((copy == 'c'))
+		total = total + file_data_process(copy,va_arg(args,int));
+	else if((copy == ''))
+		total = total + (copy,va_arg(args,type));	
+
+	//!else()
+	//! this is doing a next time, because i need time to fixed
+	return(total);
+}
+
+static int core(va_list args, const char *format)
+{
+	int	total;
+	
+	total = 0;
+	while(*format)
+	{
+		if(*format == '%')
+		{
+			total = data_processing(args,total,*(format + 1));
+			format++;
+		}
+		else
+		{
+			total = totalprint_char(*(format));
+			format++;
+		}
+	}
+	return(total);
+}
 
 int ft_printf(const char *format, ...)
 {
-	va_list args;
-    int     x;
-    
-    x = 0;
-    va_start(args,format);
-    while()
-    {
-        
-    }
-    
-    
-    
+	va_list		args;
+	int			total_length_doin;
+	
+	va_start (args, format);
+	total_length_doin = core(args,format);
+	va_end (args);
+	return (total_length_doin);
 }
